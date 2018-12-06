9721a03b0ae182ff026af0dcf833ba71f14bea70
dhyana1984 <xiongyi_1984@foxmail.com>
2018-12-06 22:51:32 +0800

    TypeJavascript

---
+++ JavascriptPrimer/primer.js
@@ -0,0 +1,7 @@
+console.log("Hello");
+console.log("Apples");
+var messageFunction = function (wether) {
+    var message = "It is " + wether + " today";
+    console.log(message);
+};
+messageFunction("raining");
---
+++ JavascriptPrimer/primer.ts
@@ -0,0 +1,9 @@
+console.log("Hello");
+console.log("Apples");
+
+let messageFunction = function(wether){
+    let message= `It is ${wether} today`
+    console.log(message)
+}
+
+messageFunction("raining");
