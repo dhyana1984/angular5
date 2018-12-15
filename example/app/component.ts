94396890e49d6fd04216eb0ae8d9a85784851af5
dhyana1984 <xiongyi_1984@foxmail.com>
2018-12-15 23:27:07 +0800

    设置class和style绑定

--- example/app/component.ts
+++ example/app/component.ts
@@ -1,5 +1,6 @@
-import {Component} from "@angular/core"
+import {Component,ApplicationRef} from "@angular/core"
 import {Model} from "./repository.model"
+import {Product} from "./product.model";
 
 @Component({
     selector:"app",
@@ -8,7 +9,47 @@ import {Model} from "./repository.model"
 export class ProductComponent{
     model: Model = new Model();
 
-    getClasses():string {
-        return this.model.getProducts().length ==5 ?"bg-success":"bg-warning";
+    constructor(ref:ApplicationRef){
+       // <any>window 全局变量
+        (<any>window).appRef=ref;
+        (<any>window).model = this.model;
+    }
+
+    getProductByPosition(position:number) :Product{
+        return this.model.getProducts()[position];
+    }
+
+    getClassesByPosition(position:number):string{
+        let product = this.getProductByPosition(position);
+        return "p-a-1 " + (product.price <50 ? "bg-info" : "bg-warning");
+    }
+
+    getClasses(key: number):string {
+        let product = this.model.getProduct(key);
+        // return this.model.getProducts().length ==5 ?"bg-success":"bg-warning";
+        return "p-a-1 " + (product.price <50? "bg-info" : "bg-warning")
+    }
+
+    //class绑定映射
+    getClassMap(key:number):Object {
+        let product = this.model.getProduct(key);
+        return {
+            "text-xs-center bg-danger" : product.name =="Kayak",
+            "bg-info" : product.price<50
+        };
+    }
+
+    fontSizeWithUnits: string = "30px";
+    fontSizeWithoutUnits: string ="30";
+
+    getStyles(key : number){
+        let product = this.model.getProduct(key);
+
+        //可以是js的属性名称格式也可以是css的属性名称格式
+        return {
+            fontSize :"30px", 
+            "margin.px":100,
+            color:product.price>50?"red":"green"
+        }
     }
 }
