bec3e04038d4999057e3df12c889933664b62f78
dhyana1984 <xiongyi_1984@foxmail.com>
2018-12-18 16:56:38 +0800

    表单和表单验证

--- example/app/component.js
+++ example/app/component.js
@@ -10,9 +10,14 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 };
 var core_1 = require("@angular/core");
 var repository_model_1 = require("./repository.model");
+var product_model_1 = require("./product.model");
+var form_model_1 = require("./form.model");
 var ProductComponent = (function () {
     function ProductComponent(ref) {
         this.model = new repository_model_1.Model();
+        this.form = new form_model_1.ProductFormGroup(); //使用表单模型
+        this.newProduct = new product_model_1.Product();
+        this.formSubmitted = false;
         // <any>window 全局变量
         window.appRef = ref;
         window.model = this.model;
@@ -25,6 +30,55 @@ var ProductComponent = (function () {
     };
     ProductComponent.prototype.getSelected = function (product) {
         return product.name == this.selectedProduct;
+    };
+    Object.defineProperty(ProductComponent.prototype, "jsonProduct", {
+        get: function () {
+            return JSON.stringify(this.newProduct);
+        },
+        enumerable: true,
+        configurable: true
+    });
+    ProductComponent.prototype.addProduct = function (p) {
+        console.log("New Product: " + this.jsonProduct);
+    };
+    //为每种错误生成验证消息
+    ProductComponent.prototype.getValidationMessage = function (state, thingName) {
+        var thing = state.path || thingName; //如果没有收到参数，则使用path属性作为描述性字符
+        var message = [];
+        if (state.errors) {
+            for (var errorName in state.errors) {
+                switch (errorName) {
+                    case "required":
+                        message.push("You must enter a " + thing);
+                        break;
+                    case "minlength":
+                        message.push("A " + thing + " must be at least " + state.errors['minlength'].requiredLength);
+                        break;
+                    case "pattern":
+                        message.push("The " + thing + " contains illegal characters");
+                        break;
+                }
+            }
+        }
+        return message;
+    };
+    //遍历表单元素，生成表单范围验证消息
+    ProductComponent.prototype.getFormValidationMessages = function (form) {
+        var _this = this;
+        var messages = [];
+        Object.keys(form.controls).forEach(function (k) {
+            _this.getValidationMessage(form.controls[k], k).forEach(function (m) { return messages.push(m); });
+        });
+        return messages;
+    };
+    ProductComponent.prototype.submitForm = function (form) {
+        this.formSubmitted = true;
+        if (form.valid) {
+            this.addProduct(this.newProduct);
+            this.newProduct = new product_model_1.Product();
+            form.reset();
+            this.formSubmitted = false;
+        }
     };
     ProductComponent = __decorate([
         core_1.Component({
--- example/app/component.ts
+++ example/app/component.ts
@@ -1,6 +1,11 @@
 import {Component,ApplicationRef} from "@angular/core"
 import {Model} from "./repository.model"
 import {Product} from "./product.model";
+import {NgForm} from "@angular/forms"
+import { NgFor } from "@angular/common";
+import {ProductFormGroup} from "./form.model"
+
+
 
 @Component({
     selector:"app",
@@ -8,6 +13,7 @@ import {Product} from "./product.model";
 })
 export class ProductComponent{
     model: Model = new Model();
+    form: ProductFormGroup = new ProductFormGroup();//使用表单模型
 
     constructor(ref:ApplicationRef){
        // <any>window 全局变量
@@ -26,6 +32,59 @@ export class ProductComponent{
     getSelected(product:Product):boolean{
 
         return product.name ==this.selectedProduct;
+    }
+
+    newProduct :Product = new Product();
+    get jsonProduct(){
+        return JSON.stringify(this.newProduct);
+    }
+
+    addProduct(p:Product){
+        console.log("New Product: " + this.jsonProduct)
+    }
+    //为每种错误生成验证消息
+    getValidationMessage(state:any, thingName?:string){
+        let thing:string = state.path || thingName;//如果没有收到参数，则使用path属性作为描述性字符
+        let message: string[] = []
+        if(state.errors){
+            for(let errorName in state.errors){
+                switch(errorName){
+                    case "required":
+                    message.push(`You must enter a ${thing}`);
+                    break;
+
+                    case "minlength":
+                    message.push(`A ${thing} must be at least ${state.errors['minlength'].requiredLength}`)
+                    break;
+
+                    case "pattern":
+                    message.push(`The ${thing} contains illegal characters`);
+                    break;
+                }
+            }
+        }
+        return message;
+    }
+
+    //遍历表单元素，生成表单范围验证消息
+    getFormValidationMessages(form:NgForm):string[]{
+        let messages :string[]= [];
+        Object.keys(form.controls).forEach(k =>{
+            this.getValidationMessage(form.controls[k],k).forEach(m =>messages.push(m));
+        });
+        return messages;
+    }
+
+    formSubmitted: boolean = false;
+
+    submitForm(form:NgForm){    //form参数实际上就是模板引用变量被赋值的ngForm
+        this.formSubmitted=true;
+        if(form.valid){
+            this.addProduct(this.newProduct);
+            this.newProduct = new Product();
+            form.reset();
+            this.formSubmitted= false;
+        }
     }
 
 }
