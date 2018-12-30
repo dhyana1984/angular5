df17d21f06396b2b77cdbd5bfaeba20488383676
dhyana1984 <xiongyi_1984@foxmail.com>
2018-12-31 00:03:33 +0800

    使用rxjs

--- example_RxJS/src/app/core/form.component.ts
+++ example_RxJS/src/app/core/form.component.ts
@@ -1,30 +1,48 @@
-import { Component} from "@angular/core";
+import { Component, Inject} from "@angular/core";
 import { NgForm} from "@angular/forms";
 import { Product } from "../model/product.model";
 import {Model} from "../model/repository.model";
-import {MODES,SharedState} from "./sharedState.model"
-
+import {MODES,SharedState,SHARED_STATE} from "./sharedState.model"
+import {Observable} from "rxjs/Observable"
+import "rxjs/add/operator/filter"
+import "rxjs/add/operator/map"
+import "rxjs/add/operator/distinctUntilChanged";
+import "rxjs/add/operator/skipWhile"
 
 @Component({
     selector: "paForm",
+    moduleId: module.id,
     templateUrl: "form.component.html",
     styleUrls:["form.component.css"]
 })
 export class FormComponent {
     product:Product= new Product();
-    constructor(private model:Model, private state:SharedState){
+    lastId:number;
+    constructor(private model:Model, 
+        //组件声明了一个对SHARED_STATE服务的依赖，这个依赖被当做一个Observable<SharedState>接收
+        //stateEvents这个对象是一个Observable，其通知则是SharedState对象，代表用户发起的编辑或者创建操作
+        //组件调用了stateEvents.subscribe方法，提供了一个函数来接收每一个SharedState对象，并使用这个对象来更新组件状态
+        @Inject(SHARED_STATE) private stateEvents: Observable<SharedState>){
+        stateEvents
+            //忽略事件，慎用
+            .skipWhile(state => state.mode==MODES.EDIT)
+            //.map(state => state.mode == MODES.EDIT? state.id : -1)
+            //点击edit以后，修改字段，然后再点击edit，修改不会丢失
+            //distinctUntilChanged不知道如何比较对象总是假定任何对象都是不同，所以可以自定义一个比较函数
+            .distinctUntilChanged((firstState, secondState) => firstState.mode == secondState.mode && firstState.id==secondState.id) 
+            // .filter(id => id!=3)
+            .subscribe(update => {
+            this.product = new Product();
+            if(update.id != undefined){
+                Object.assign(this.product, this.model.getProduct(update.id));
+            }
+            this.editing = update.mode == MODES.EDIT;
+        })
        
     }
-    get editing(): boolean{
-        return this.state.mode==MODES.EDIT;
-    }
-
-
-
+    editing : boolean =false;
     submitForm(form: NgForm) {
         if (form.valid) {
-       
-            console.log(this.product.id)
             this.model.saveProduct(this.product);
             this.product = new Product();
             form.reset();
@@ -33,4 +51,15 @@ export class FormComponent {
     resetForm(){
         this.product = new Product();
     }
+    //使用ngDoCheck可能会在状态变更时产生问题，以及难以维护复杂逻辑的变更
+    // ngDoCheck(): void {
+    //     if(this.lastId != this.state.id){
+    //         this.product = new Product();
+    //         if(this.state.mode == MODES.EDIT){
+    //             Object.assign(this.product, this.model.getProduct(this.state.id));
+    //         }
+    //         this.lastId = this.state.id;
+    //     }
+        
+    // }
 }
--- example_RxJS/src/app/core/table.Component.ts
+++ example_RxJS/src/app/core/table.Component.ts
@@ -1,7 +1,8 @@
-import {Component} from "@angular/core";
+import {Component, Inject} from "@angular/core";
 import {Model} from "../model/repository.model";
 import {Product} from "../model/product.model";
-import {MODES,SharedState} from "./sharedState.model"
+import {MODES,SharedState,SHARED_STATE} from "./sharedState.model"
+import {Observer} from "rxjs/Observer"
 
 @Component({
     selector:"paTable",
@@ -10,7 +11,10 @@ import {MODES,SharedState} from "./sharedState.model"
 })
 export class TableComponent{
       
-    constructor(private datamodel:Model,private state:SharedState){
+    constructor(private datamodel:Model,
+                //组建生命了一个对SHARED_STATE服务的依赖，SHARED_STATE服务是作为一个Observer<SharedState>对象的接收
+                //observer将会发送试用SharedState对象描述的事件
+                @Inject(SHARED_STATE) private observer: Observer<SharedState>){
 
     }
 
@@ -24,13 +28,15 @@ export class TableComponent{
         this.datamodel.deleteProduct(key);
     }
     editProduct(key: number){
-      
-       this.state.id = key;
-       this.state.mode=MODES.EDIT;
+    //    this.state.id = key;
+    //    this.state.mode=MODES.EDIT;
+        //observer的next方法指定用new SharedState(MODES.EDIT,key)创建一个新事件
+        this.observer.next(new SharedState(MODES.EDIT,key));
     }
     createProduct(){
-        this.state.id = undefined;
-       this.state.mode=MODES.CREATE; 
+    //    this.state.id = undefined;
+    //    this.state.mode=MODES.CREATE; 
+    this.observer.next(new SharedState(MODES.CREATE));
     }
 
 }
